c612b1d246bf1e2306ed555adcce790cea014f69
Marc Reyes <marc.reyes@pointwest.com.ph>
2017-11-20 19:52:04 +0800

    Add DayOneMain,java

---
+++ DayOneMain.java
@@ -0,0 +1,25 @@
+public class DayOneMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Buy milk and eggs
+	int yearInPTC=7;
+	boolean iHaveAPickle=true;
+	double distanceToYourHeart=3944.44;
+	String myFavoirteDrink = "Coke Zero";
+	String yourFavoriteFood;
+	// Scanner borFargh = new Scanner(System.in);
+		char theBestLetter='C';
+		System.out.println("Hallo Worrrod");
+		System.out.println("i am bad");
+		System.out.println("Years in Pointwest " + yearInPTC);
+		System.out.println("Do I have a pickle? "+ iHaveAPickle);
+		System.out.println("Distance to your Heart "+distanceToYourHeart);
+		System.out.println("The Best Letter " + theBestLetter);
+		System.out.println("My Favorite Drink "+myFavoirteDrink);
+		System.out.println("What is your favorite food?");
+		// yourFavoriteFood = borFargh.nextLine();
+		// System.out.println("Your favorite Food is "+yourFavoriteFood);
+	}
+
+}
